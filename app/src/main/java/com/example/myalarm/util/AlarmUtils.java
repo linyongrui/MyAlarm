2a5ebd644d7d2bf02d60c5ddfd3fa89d19f5c490
linyongrui <lin_yongrui@163.com>
2025-06-07 00:13:47 +0800

    frontend

--- app/src/main/java/com/example/myalarm/util/AlarmUtils.java
+++ app/src/main/java/com/example/myalarm/util/AlarmUtils.java
@@ -1,11 +1,9 @@
 package com.example.myalarm.util;
 
-import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
 
 import com.example.myalarm.alarmtype.BaseAlarmType;
@@ -210,7 +208,6 @@ public class AlarmUtils {
         return nextTriggerTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 
-    @SuppressLint("ScheduleExactAlarm")
     public static void setAlarm(Context context, AlarmEntity alarm) {
         long triggerTime = getNextTriggerTime(alarm);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
@@ -228,25 +225,7 @@ public class AlarmUtils {
                 PendingIntent.FLAG_IMMUTABLE
         );
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            alarmManager.setExactAndAllowWhileIdle(
-                    AlarmManager.RTC_WAKEUP,
-                    triggerTime,
-                    pendingIntent
-            );
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            alarmManager.setExact(
-                    AlarmManager.RTC_WAKEUP,
-                    triggerTime,
-                    pendingIntent
-            );
-        } else {
-            alarmManager.set(
-                    AlarmManager.RTC_WAKEUP,
-                    triggerTime,
-                    pendingIntent
-            );
-        }
+        alarmManager.setExactAndAllowWhileIdle( AlarmManager.RTC_WAKEUP, triggerTime, pendingIntent);
     }
 
     public static void cancelAlarm(Context context, int alarmId) {
